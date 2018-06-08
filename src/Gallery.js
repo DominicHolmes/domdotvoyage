ac17570489b94857ca03124a882b1a5e50145c19
dom <hello@dom.voyage>
2018-06-07 22:37:17 -0500

    initial commit

---
+++ src/Gallery.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Gallery.css';
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div className="Gallery-header">
+        <div className="Gallery-content">
+          <h2>Gallery content</h2>
+          <h3>More content here</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
